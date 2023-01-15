7c8c2be7b12d178c62037e092d11714b15860e3c
Maciej Pawłowski <maciekpawlowski1@gmail.com>
2023-01-15 18:49:40 +0100

    Added some tests of AppRepository

--- app/src/test/java/com/pawlowski/sportnite/domain/AppRepositoryTest.kt
+++ app/src/test/java/com/pawlowski/sportnite/domain/AppRepositoryTest.kt
@@ -1,34 +1,82 @@
-@file:OptIn(ExperimentalCoroutinesApi::class)
 
 package com.pawlowski.sportnite.domain
 
-import io.mockk.mockk
+import com.dropbox.android.external.store4.Store
+import com.pawlowski.sportnite.data.auth.IAuthManager
+import com.pawlowski.sportnite.data.auth.UserInfoUpdateCache
+import com.pawlowski.sportnite.data.firebase_storage.FirebaseStoragePhotoUploader
+import com.pawlowski.sportnite.data.local.MeetingsInMemoryCache
+import com.pawlowski.sportnite.data.local.OffersInMemoryCache
+import com.pawlowski.sportnite.data.local.OffersToAcceptMemoryCache
+import com.pawlowski.sportnite.data.remote.IGraphQLService
+import com.pawlowski.sportnite.domain.models.MeetingsFilter
+import com.pawlowski.sportnite.domain.models.OffersFilter
+import com.pawlowski.sportnite.domain.models.PlayersFilter
+import com.pawlowski.sportnite.presentation.models.*
+import com.pawlowski.sportnite.utils.Resource
+import com.pawlowski.sportnite.utils.onError
+import io.mockk.*
+import io.mockk.impl.annotations.RelaxedMockK
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.StandardTestDispatcher
+import kotlinx.coroutines.test.TestCoroutineScheduler
 import kotlinx.coroutines.test.runTest
+import org.junit.Before
 import org.junit.Test
 
+@OptIn(ExperimentalCoroutinesApi::class)
 internal class AppRepositoryTest {
+
+
+    @RelaxedMockK
+    private lateinit var userInfoUpdateCacheMock: UserInfoUpdateCache
+    @RelaxedMockK
+    private lateinit var authManager: IAuthManager
+    @RelaxedMockK
+    private lateinit var firebaseStoragePhotoUploader: FirebaseStoragePhotoUploader
+    @RelaxedMockK
+    private lateinit var graphQLService: IGraphQLService
+    @RelaxedMockK
+    private lateinit var playersStore: Store<PlayersFilter, List<Player>>
+    @RelaxedMockK
+    private lateinit var offersStore: Store<OffersFilter, List<GameOffer>>
+    @RelaxedMockK
+    private lateinit var gameOffersToAcceptStore: Store<OffersFilter, List<GameOfferToAccept>>
+    @RelaxedMockK
+    private lateinit var playerDetailsStore: Store<String, PlayerDetails>
+    @RelaxedMockK
+    private lateinit var meetingsStore: Store<MeetingsFilter, List<Meeting>>
+    @RelaxedMockK
+    private lateinit var meetingsInMemoryCache: MeetingsInMemoryCache
+    @RelaxedMockK
+    private lateinit var offersInMemoryCache: OffersInMemoryCache
+    @RelaxedMockK
+    private lateinit var offersToAcceptMemoryCache: OffersToAcceptMemoryCache
+
+    @Before
+    fun setUp() = MockKAnnotations.init(this, relaxUnitFun = true) // turn relaxUnitFun on for all mocks
+
+
+    private fun getSUT(testScheduler: TestCoroutineScheduler): AppRepository {
+        return AppRepository(
+            userInfoUpdateCache = userInfoUpdateCacheMock,
+            authManager = authManager,
+            firebaseStoragePhotoUploader = firebaseStoragePhotoUploader,
+            ioDispatcher = StandardTestDispatcher(scheduler = testScheduler, "ioDispatcher"),
+            playersStore = playersStore,
+            offersStore = offersStore,
+            gameOffersToAcceptStore = gameOffersToAcceptStore,
+            playerDetailsStore = playerDetailsStore,
+            meetingsStore = meetingsStore,
+            meetingsInMemoryCache = meetingsInMemoryCache,
+            offersInMemoryCache = offersInMemoryCache,
+            offersToAcceptMemoryCache = offersToAcceptMemoryCache,
+            graphQLService = graphQLService
+        )
+    }
 
     @Test
     fun getIncomingMeetings() = runTest {
-        val sut = AppRepository(
-            userInfoUpdateCache = mockk(),
-            authManager = mockk(),
-            firebaseStoragePhotoUploader = mockk(),
-            ioDispatcher = StandardTestDispatcher(scheduler = testScheduler, "ioDispatcher"),
-            playersStore = mockk(),
-            offersStore = mockk(),
-            gameOffersToAcceptStore = mockk(),
-            playerDetailsStore = mockk(),
-            meetingsStore = mockk(),
-            meetingsInMemoryCache = mockk(),
-            offersInMemoryCache = mockk(),
-            offersToAcceptMemoryCache = mockk(),
-            graphQLService = mockk()
-        )
-        val result = sut.getIncomingMeetings(sportFilter = null)
-
 
     }
 
@@ -73,11 +121,49 @@ internal class AppRepositoryTest {
     }
 
     @Test
-    fun sendOfferToAccept() {
+    fun sendOfferToAccept() = runTest {
+        val sut = getSUT(testScheduler)
+        coEvery {
+            graphQLService.sendOfferToAccept(any())
+        } returns Resource.Success("id")
+
+        val offerId = "offerId"
+        val result = sut.sendOfferToAccept(offerUid = offerId)
+
+        result.onError { _, _ -> assert(false) }
+        coVerify {
+            graphQLService.sendOfferToAccept(offerUid = offerId)
+        }
+
+        coVerify {
+            offersInMemoryCache.updateElements(any())
+        }
     }
 
     @Test
-    fun acceptOfferToAccept() {
+    fun acceptOfferToAccept() = runTest {
+        //TODO: find out how to test Store
+//        val sut = getSUT(testScheduler)
+//        coEvery {
+//            graphQLService.acceptOfferToAccept(any())
+//        } returns Resource.Success(Unit)
+//
+//        coEvery {
+//            meetingsStore.fresh(any())
+//        } returns listOf()
+//
+//        val offerToAcceptId = "offerToAcceptId"
+//        val result = sut.acceptOfferToAccept(offerToAcceptUid = offerToAcceptId)
+//
+//        result.onError { _, _ -> assert(false) }
+//        coVerify {
+//            graphQLService.sendOfferToAccept(offerUid = offerToAcceptId)
+//        }
+//
+//
+//        coVerify {
+//            offersInMemoryCache.deleteElementFromAllKeys(any())
+//        }
     }
 
     @Test
